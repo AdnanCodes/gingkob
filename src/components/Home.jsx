8c017d90a2bcd1b3e32409f1649fb3d007ba0df5
Adnan <adnan.chowdhury@outlook.com>
2020-08-09 18:05:18 -0500

    Title and Nav Comps built

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import Navigation from "./Nav.jsx";
 // import { Link } from "react-router-dom";
+import "./Home.scss";
+//Material UI
+import { Typography, Container, Grid } from "@material-ui/core";
+/* TODO
+  1. Carousel (display your top items)
+	2. Mini Intro (Picture & 1-2 sentence welcome message)
+	3. Bottom of screen more Contact Info and Copyright
+  4. Navigation Bar (Could be breadcrumbs or others) Containing links to other pages (All for development)
+  5. Clickable to products Requires Product Page and dedicated product component which creates Full UI for one product 
+*/
 
 const Home = () => {
   return (
-    <div className="Home">
-      <h1> Home Page </h1>
-    </div>
+    <Container>
+      <Grid
+        container
+        display="flex"
+        alignContent="center"
+        justify="space-evenly"
+      >
+        <Typography align="center" variant="h1">
+          Gingko B.
+        </Typography>
+        <Navigation />
+      </Grid>
+      <section className="intro">
+        Welcome to my site! <br />
+        Hi! I am Autumn and I work in Fiber Art, Knitwear Design, and Handmade
+        Wardrobe
+      </section>
+    </Container>
   );
 };
-
 export default Home;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  //   paper: {
+  //     marginRight: theme.spacing(2),
+  //   },
+}));
+
+const Navigation = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div className={classes.root}>
+      <Button
+        ref={anchorRef}
+        aria-controls={open ? "menu-list-grow" : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+      >
+        Menu
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList autoFocusItem={open} id="menu-list-grow">
+                  <MenuItem onClick={handleClose}> Home</MenuItem>
+                  <MenuItem onClick={handleClose}>About</MenuItem>
+                  {/* <MenuItem onClick={handleClose}>Logout</MenuItem> */}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
+  );
+};
+
+export default Navigation;
