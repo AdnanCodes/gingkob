3c25ef1a15062c40318cfd74b41eac519ab3e350
Adnan <adnan.chowdhury@outlook.com>
2020-10-06 14:48:29 -0500

    git ignore fix

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./assets/tailwind.output.css";
+
+function App() {
+  return (
+    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+      <div className="ml-6 pt-1">
+        <h1 className="text-2xl text-blue-700 leading-tight">
+          Tailwind and Create React App
+        </h1>
+        <p className="text-base text-gray-700 leading-normal">
+          Building apps together
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
